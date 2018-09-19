3a70365a02d4760fad7126dc4baea00de31b960d
Shaun Toh <smurfination1@gmail.com>
2018-09-19 20:09:49 +0800

    made it work with put and delete properly

--- Start.py
+++ Start.py
@@ -2,6 +2,7 @@ from flask import request
 from flask import Flask
 from flask import jsonify
 from functools import wraps
+from flask_restful import Resource, Api
 
 # curl http://127.0.0.1:5000
 # GET /
@@ -22,7 +23,10 @@ from functools import wraps
 # curl -v -u "admin:secret" http://127.0.0.1:5000/login
 # for logins.
 
+
 app = Flask(__name__)
+api = Api(app)
+initialisedSTORAGE={}
 @app.route("/")
 def root():
     return "YES"
@@ -55,24 +59,55 @@ def failed_Auth():
 def loginstuff():
     return request.data
 
-@app.route("/users/<ID>", methods = ["GET","POST","PUT","DELETE"]) # note the keyword argument here.
-def stuff(ID):
-    if request.method == "GET": # literally shoots a string back
-        return "someinfo"
-    elif request.method =="PUT":
-        app.add_resource({}, '/<string:ID>')
-    elif request.method =="POST": # won't do anything but will send a weird response back
-        resp = jsonify("Success")
-        resp.header="aha"
-        resp.data=request.data
-        resp.status_code = 405
-        return resp
-    elif request.method=="DELETE":
-        """deletesomething"""
-    else: #not any of the above, somehow got there.
+
+# curl -v -H "Content-type: application/json" -H "Accept: application/json" http://127.0.0.1:5000/users/1  -d "{"data1":"noot", "data2":"noot"}"
+# for accepting a json response too
+#@app.route("/users/<ID>", methods = ["GET","POST","PUT","DELETE"]) # note the keyword argument here.
+
+class manythings(Resource):
+    def get(self,ID): # curl -v   http://127.0.0.1:5000/users/1
+        if request.method == "GET": # literally shoots a string back
+            if(ID in list(initialisedSTORAGE.keys())):
+                resp = jsonify("success")
+                resp.data = initialisedSTORAGE[ID]
+                resp.status_code=200
+                return resp
+            else:
+                resp = jsonify("none")
+                resp.status_code=404
+                resp.data="ERROR NO DATA\n"
+                return resp
+#curl -X PUT -H "Content-Type: application/json" -d '{"key1":"value"}' "http://127.0.0.1:5000/users/1"
+    def put(self,ID):
+        if request.method =="PUT":
+            initialisedSTORAGE[ID]=request.data
+            resp = jsonify("")
+            resp.data = initialisedSTORAGE[ID]
+            resp.status_code = 200
+            return resp # has to be jsonified
+    def post(self,ID):
+        if request.method =="POST": # won't do anything but will echo with a 405. which is pretty funny
+            resp = jsonify("")
+            resp.data =request.data
+            resp.header="aha"
+            resp.status_code = 405
+            return resp
+    def delete(self,ID): # curl http://127.0.0.1:5000/users/1 -X DELETE -v
+        if(ID in list(initialisedSTORAGE.keys())):
+            del initialisedSTORAGE[ID]
+            return '', 200
+        else:
+            resp = jsonify("")
+            resp.data = request.data
+            resp.header = "No. Nothing to delete here yet"
+            resp.status_code = 204
+            return resp
+
+
+    """else: #not any of the above, somehow got there.
         message= {"ERROR":"ERROR"}
         resp = jsonify(message)
         resp.status_code=405
         return resp
-
-
+"""
+api.add_resource(manythings, '/users/<ID>')
